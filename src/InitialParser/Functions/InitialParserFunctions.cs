7b1c253c2c024f192f4a4c4af79127610099827f
some-dev <some-dev@gmail.com>
2017-07-11 09:45:46 +0300

    setting processing status

--- src/InitialParser/Functions/InitialParserFunctions.cs
+++ src/InitialParser/Functions/InitialParserFunctions.cs
@@ -65,7 +65,7 @@ namespace InitialParser.Functions
                     blocksHeightsToParse.Add(height);
                 }
             }
-            var semaphore = new SemaphoreSlim(1);
+            var semaphore = new SemaphoreSlim(100);
 
             var cancellationTokenSource = new CancellationTokenSource();
 
