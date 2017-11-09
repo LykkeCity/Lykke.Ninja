86c582b5a77a55e2f7b3d99580145a349d1470c0
some-dev <some-dev@gmail.com>
2017-11-09 09:38:56 +0300

    put failed, stacked to queue again

--- src/Lykke.Ninja.BalanceJob/BlockTasks/BlockTasksProducerFunctions.cs
+++ src/Lykke.Ninja.BalanceJob/BlockTasks/BlockTasksProducerFunctions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Threading.Tasks;
+using Common;
 using Common.Log;
 using Lykke.JobTriggers.Triggers.Attributes;
 using Lykke.Ninja.Core.AlertNotifications;
@@ -77,14 +78,19 @@ namespace Lykke.Ninja.BalanceJob.BlockTasks
         public async Task PutFailedToQueueAgain()
         {
             try
-            {
-                _console.WriteLine($"{nameof(PutFailedToQueueAgain)} started");
-                var failedBlocks = await _blockStatusesRepository.GetList(BlockProcessingStatus.Fail, 50);
+			{
+				var queuedCommandCount = await _commandProducer.GetQueuedCommandCount();
+				if (queuedCommandCount < _baseSettings.MaxParseBlockQueuedCommandCount)
+				{
+					var failedBlocks = await _blockStatusesRepository.GetList(BlockProcessingStatus.Fail, 10);
 
-                foreach (var blockStatus in failedBlocks.Where(p => p.StatusChangedAt > DateTime.UtcNow.AddMinutes(-10)))
-                {
-                    await _commandProducer.ProduceParseBlockCommand(blockStatus.Height);
-                }
+					foreach (var blockStatus in failedBlocks.Where(p => p.StatusChangedAt < DateTime.UtcNow.AddMinutes(-60)))
+					{
+						await _commandProducer.ProduceParseBlockCommand(blockStatus.Height);
+
+						await _log.WriteInfoAsync(nameof(BlockTasksProducerFunctions), nameof(PutFailedToQueueAgain), new { height = blockStatus.Height, id = blockStatus.BlockId }.ToJson(), "Put to queue again");
+					}
+				}
             }
             catch (Exception e)
             {
@@ -93,5 +99,56 @@ namespace Lykke.Ninja.BalanceJob.BlockTasks
                 throw;
             }
         }
-    }
+
+	    [TimerTrigger("00:10:00")]
+	    public async Task PutStackedStartedToQueueAgain()
+	    {
+		    try
+		    {
+			    var queuedCommandCount = await _commandProducer.GetQueuedCommandCount();
+			    if (queuedCommandCount <_baseSettings.MaxParseBlockQueuedCommandCount)
+			    {
+				    var blocks = await _blockStatusesRepository.GetList(BlockProcessingStatus.Started, 10);
+
+				    foreach (var blockStatus in blocks.Where(p => p.StatusChangedAt < DateTime.UtcNow.AddMinutes(-60)))
+				    {
+						await _commandProducer.ProduceParseBlockCommand(blockStatus.Height);
+						await _log.WriteInfoAsync(nameof(BlockTasksProducerFunctions), nameof(PutStackedStartedToQueueAgain), new { height = blockStatus.Height, id = blockStatus.BlockId }.ToJson(), "Put to queue again");
+					}
+			    }
+		    }
+		    catch (Exception e)
+		    {
+			    await _log.WriteErrorAsync(nameof(BlockTasksProducerFunctions), nameof(PutFailedToQueueAgain), null, e);
+			    await _slack.SendError(nameof(PutFailedToQueueAgain), e.Message);
+			    throw;
+		    }
+	    }
+
+	    [TimerTrigger("00:10:00")]
+	    public async Task PutStackedQueuedToQueueAgain()
+	    {
+		    try
+		    {
+			    var queuedCommandCount = await _commandProducer.GetQueuedCommandCount();
+			    if (queuedCommandCount < _baseSettings.MaxParseBlockQueuedCommandCount)
+			    {
+				    var blocks = await _blockStatusesRepository.GetList(BlockProcessingStatus.Queued, 10);
+
+				    foreach (var blockStatus in blocks.Where(p => p.StatusChangedAt < DateTime.UtcNow.AddMinutes(-60)))
+					{
+						await _commandProducer.ProduceParseBlockCommand(blockStatus.Height);
+
+						await _log.WriteInfoAsync(nameof(BlockTasksProducerFunctions), nameof(PutStackedQueuedToQueueAgain), new { height = blockStatus.Height, id = blockStatus.BlockId }.ToJson(), "Put to queue again");
+					}
+				}
+		    }
+		    catch (Exception e)
+		    {
+			    await _log.WriteErrorAsync(nameof(BlockTasksProducerFunctions), nameof(PutFailedToQueueAgain), null, e);
+			    await _slack.SendError(nameof(PutFailedToQueueAgain), e.Message);
+			    throw;
+		    }
+	    }
+	}
 }
